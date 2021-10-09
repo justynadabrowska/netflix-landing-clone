5465ea2e62bf616fd3ef62a0855a3eb288650993
justynadabrowska <74719040+justynadabrowska@users.noreply.github.com>
2021-10-09 17:51:43 +0100

    worked on cloning the look of netflix

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@ module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        'hero-bg': "url('/img/background.jpg')"
+      }
+    },
   },
   variants: {
     extend: {},
